d1e2350bfe8639e5c31243d369dadc3349754157
nosaxxx <128120498+nosaxxx@users.noreply.github.com>
2025-06-17 13:55:32 +0800

    Configure build output to docs folder and rebuild

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,18 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react( ),tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  // 請在這裡添加或修改以下兩行：
+  base: 	'/hongsing-website/', // 請將 'hongsing-website' 替換為您的GitHub倉庫名稱
+  build: {
+    outDir: 'docs' // 將這裡的 'dist' 改為 'docs'
+  }
 })
+
